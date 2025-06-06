21f99e836b91216fbf8cc42550297eb82d1364ec
Aradhana Kathrotiya <sojitra98aradhana@gmail.com>
2025-06-06 09:59:50 +0530

    c-2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "hello Express app..." });
+  res.json({ message: "hii,hello Express app..." });
 });
 
 app.listen(8000, () => console.log("server started"));
